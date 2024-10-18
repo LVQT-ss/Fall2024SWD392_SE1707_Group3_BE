e68d0e6f9bc1a54e394518c1abd1fb924e527784
LukeDragnar <trandainghia291203@gmail.com>
2024-10-18 11:15:54 +0700

    thêm chức năng get all by id cho category - Trần Đại Nghĩa

--- server/src/controller/category.controller.js
+++ server/src/controller/category.controller.js
@@ -44,4 +44,30 @@ export const getAllCategories = async (req, res) => {
   }
 };
 
+// Lấy danh mục theo ID
+export const getCategoryById = async (req, res) => {
+  const { categoryId } = req.params;
+
+  if (!categoryId) {
+    return res.status(400).json({ message: 'Category ID is required' });
+  }
+
+  try {
+    const category = await Category.findByPk(categoryId, {
+      include: {
+        model: Product,
+        attributes: ['productId', 'productName'],
+      },
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    console.error('Error fetching category:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
